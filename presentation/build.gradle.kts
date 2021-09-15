db8f1c2b74b203424bad880a7c97d938bee81060
inorichi <len@kanade.eu>
2021-09-15 16:16:43 +0200

    Convert presentation to multiplatform module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,56 +1,57 @@
 plugins {
+  kotlin("multiplatform")
   id("com.android.library")
   id("com.mikepenz.aboutlibraries.plugin")
-  id("kotlin-android")
   id("kotlin-kapt")
+  id("org.jetbrains.compose")
   id("org.jetbrains.gradle.plugin.idea-ext")
 }
 
-android {
-  kotlinOptions {
-    jvmTarget = JavaVersion.VERSION_1_8.toString()
+kotlin {
+  android()
+  sourceSets {
+    named("commonMain") {
+      dependencies {
+        implementation(Deps.androidx.compose.material)
+        implementation(Deps.androidx.compose.tooling)
+      }
+    }
+    named("androidMain") {
+      kotlin.srcDir("src/sharedJvmMain/kotlin")
+      dependencies {
+        implementation(Deps.androidx.browser)
+        implementation(Deps.androidx.webkit)
+        implementation(Deps.androidx.emoji)
+
+        implementation(Deps.androidx.compose.ui)
+        implementation(Deps.androidx.compose.icons)
+        implementation(Deps.androidx.compose.navigation)
+
+        implementation(Deps.accompanist.pager)
+        implementation(Deps.accompanist.pagerIndicator)
+        implementation(Deps.accompanist.flowlayout)
+        implementation(Deps.accompanist.insets)
+        implementation(Deps.accompanist.systemUiController)
+        implementation(Deps.accompanist.swipeRefresh)
+        implementation(Deps.accompanist.navAnimation)
+
+        implementation(Deps.coil.core)
+        implementation(Deps.coil.compose)
+
+        implementation(Deps.toothpick.runtime)
+        implementation(Deps.toothpick.ktp)
+
+        implementation(Deps.aboutLibraries.core)
+      }
+      project.dependencies.apply {
+        implementationProject(Projects.core)
+        implementationProject(Projects.sourceApi)
+        implementationProject(Projects.domain)
+        implementationProject(Projects.uiCore)
+        add("kapt", Deps.toothpick.compiler)
+      }
+    }
   }
-  buildFeatures {
-    compose = true
-  }
-  composeOptions {
-    kotlinCompilerExtensionVersion = Deps.androidx.compose.version
-  }
-}
-
-dependencies {
-  implementationProject(Projects.core)
-  implementationProject(Projects.sourceApi)
-  implementationProject(Projects.domain)
-  implementationProject(Projects.uiCore)
-
-  implementation(Deps.androidx.browser)
-  implementation(Deps.androidx.webkit)
-  implementation(Deps.androidx.emoji)
-
-  implementation(Deps.androidx.compose.compiler)
-  implementation(Deps.androidx.compose.ui)
-  implementation(Deps.androidx.compose.tooling)
-  implementation(Deps.androidx.compose.material)
-  implementation(Deps.androidx.compose.icons)
-  implementation(Deps.androidx.compose.navigation)
-
-  implementation(Deps.accompanist.pager)
-  implementation(Deps.accompanist.pagerIndicator)
-  implementation(Deps.accompanist.flowlayout)
-  implementation(Deps.accompanist.insets)
-  implementation(Deps.accompanist.systemUiController)
-  implementation(Deps.accompanist.swipeRefresh)
-  implementation(Deps.accompanist.navAnimation)
-
-  implementation(Deps.coil.core)
-  implementation(Deps.coil.compose)
-
-  implementation(Deps.toothpick.runtime)
-  implementation(Deps.toothpick.ktp)
-  kapt(Deps.toothpick.compiler)
-
-  implementation(Deps.aboutLibraries.core)
 }
 
 idea {
