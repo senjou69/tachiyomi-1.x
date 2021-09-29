5b4a92fa6b1e94f638a90214be5c8bdf1173349c
inorichi <len@kanade.eu>
2021-09-29 10:08:37 +0200

    Add JVM implementation for preferencees

--- core/src/commonMain/kotlin/util/Codec.kt
+++ core/src/commonMain/kotlin/util/Codec.kt
@@ -10,7 +10,10 @@ package tachiyomi.core.util
 
 import okio.ByteString.Companion.decodeBase64
 import okio.ByteString.Companion.encodeUtf8
+import okio.ByteString.Companion.toByteString
 
 fun String.decodeBase64() = decodeBase64()!!
 
 fun String.md5() = encodeUtf8().md5().hex()
+
+fun String.encodeBase64() = encodeToByteArray().toByteString().base64()
