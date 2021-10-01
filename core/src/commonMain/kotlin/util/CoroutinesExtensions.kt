7d36107b3673aeb7cf93ff6a6d12791c6303595b
inorichi <len@kanade.eu>
2021-10-01 18:23:20 +0200

    Add coroutine extension to support actors from common code

---
+++ core/src/commonMain/kotlin/util/CoroutinesExtensions.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2018 The Tachiyomi Open Source Project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package tachiyomi.core.util
+
+import kotlinx.coroutines.CompletionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * TODO(inorichi): this is temporary until actors are implemented in multiplatform:
+ *   https://github.com/Kotlin/kotlinx.coroutines/issues/1851
+ */
+expect class ActorScope<E> {
+  val channel: Channel<E>
+}
+
+expect fun <E> CoroutineScope.actor(
+  context: CoroutineContext,
+  capacity: Int = 0,
+  start: CoroutineStart = CoroutineStart.DEFAULT,
+  onCompletion: CompletionHandler? = null,
+  block: suspend ActorScope<E>.() -> Unit
+): SendChannel<E>
